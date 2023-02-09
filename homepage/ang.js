0ca027d1a1c892805b7616dbbb5838128fc0fa98
f22ba101 <116255761+f22ba101@users.noreply.github.com>
2023-02-09 13:34:09 +0900

    Add files via upload

---
+++ homepage/ang.js
@@ -0,0 +1,16 @@
+// みないでねｗ
+function button() {
+    let kaidoku = document.getElementById("a").value;
+    
+
+
+
+    if (kaidoku == "KOUKINOJUGYOUTOTEMOTANOSIKATTA") {
+        location.href = "pro.html";
+
+    }
+    else {
+        alert("残念もう1回頑張ってね");
+        
+    }
+}
